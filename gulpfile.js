8c63be3042994fc2f6ff10ad3b844c6d2d8f9e24
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-08-12 18:42:41 -0700

    Initial version. Everything works.

---
+++ gulpfile.js
@@ -0,0 +1,156 @@
+/// <reference path="./typings/tsd.d.ts" />
+
+var gulp        = require('gulp'),
+    ts          = require('gulp-typescript'),
+    merge       = require('merge-stream'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    git         = require('gulp-git'),
+    bump        = require('gulp-bump'),
+    tag_version = require('gulp-tag-version'),
+    filter      = require('gulp-filter'),
+    mocha       = require('gulp-mocha'),
+    del         = require('del'),
+    runSequence = require('run-sequence'),
+    tsd         = require('gulp-tsd'),
+    nodemon     = require('gulp-nodemon');
+    
+var PATHS = {
+  src: 'lib',
+  examples: 'examples',
+  build: 'build',
+  test: 'test',
+  typings: 'typings'
+};
+ 
+var tsProject = ts.createProject('tsconfig.json', { sortOutput: true });
+ 
+/**
+ * Dev tasks
+ */
+gulp.task('tsd', function (callback) {
+  tsd({
+    command: 'reinstall',
+    config: './tsd.json'
+  }, callback);
+});
+
+gulp.task('clean:tsd', function (cb) {
+  del([
+    PATHS.typings
+  ], cb);
+});
+
+gulp.task('scripts:dev', function() {
+  var tsResult = gulp.src([
+      PATHS.src + '/**/*.ts',
+      PATHS.test + '/**/*.ts',
+      PATHS.examples + '/**/*.ts'
+    ], { base: "./" })
+      .pipe(sourcemaps.init())
+      .pipe(ts(tsProject));
+   
+  return merge([
+    tsResult.js
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('.'))
+  ]);
+});
+gulp.task('scripts:dev:watch', ['scripts:dev'], function () {
+  gulp.watch([
+    PATHS.src + '/**/*.ts',
+    PATHS.test + '/**/*.ts',
+    PATHS.examples + '/**/*.ts'
+  ], ['scripts:dev']);
+});
+
+gulp.task('clean:dev', function (cb) {
+  del([
+    PATHS.src + '/**/*.js',
+    PATHS.test + '/**/*.js'
+  ], cb);
+});
+
+/**
+ * Tests tasks
+ */
+gulp.task('test', ['scripts:dev'], function () {
+  return gulp.src(PATHS.test + '/**/*.js', {read: false})
+    .pipe(mocha({
+      reporter: 'spec'
+    }));
+});
+
+gulp.task('test:watch', ['test'], function() {
+    gulp.watch([
+      PATHS.src + '/**/*.ts',
+      PATHS.test + '/**/*.ts'
+    ], ['test']);
+});
+
+/**
+ * Prod
+ */
+gulp.task('scripts:prod', function() {
+  var tsResult = gulp.src([
+      PATHS.src + '/**/*.ts'
+    ])
+      .pipe(sourcemaps.init())
+      .pipe(ts(tsProject));
+   
+  return merge([
+    tsResult.dts.pipe(gulp.dest(PATHS.build + '/definitions')),
+    tsResult.js
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(PATHS.build + '/js'))
+  ]);
+}); 
+
+gulp.task('clean:prod', function (cb) {
+  del([
+    PATHS.build
+  ], cb);
+});
+
+/**
+ * Cleaning
+ */
+gulp.task('clean', ['clean:dev', 'clean:prod', 'clean:tsd']);
+
+/**
+ * Default
+ */
+gulp.task('default', function (cb) {
+  runSequence(
+    'clean',
+    'tsd',
+    'test',
+    'scripts:prod'
+  );
+});
+
+/**
+ * CI
+ */
+gulp.task('ci', function (cb) {
+  runSequence(
+    'clean',
+    'tsd',
+    'test'
+  );
+});
+
+/**
+ * Bumping version
+ */
+function inc(importance) {
+  return gulp.src(['./package.json'])
+    .pipe(bump({type: importance}))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('Bumps for new ' + importance + ' release.'))
+    .pipe(filter('package.json'))
+    .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
---
+++ lib/nervous-sigmoid.ts
@@ -0,0 +1,4 @@
+declare module 'nervous-sigmoid' {
+  export function sigmoid(z: number): number;
+  export function sigmoidPrime(z: number): number;
+}
